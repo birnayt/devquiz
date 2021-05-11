4bd0ee518a9909276a231a4532a6f9b21d552a8e
LucasFr <lucasfariasrodrigues25@gmail.com>
2021-05-11 16:12:11 -0300

    Add layout of quiz page and its widgets

---
+++ lib/shared/widgets/decoration/box_decoration_widget.dart
@@ -0,0 +1,31 @@
+// import 'package:flutter/material.dart';
+
+// class CustomBoxDecoration {
+//   final double width;
+//   final double height;
+//   final double radius;
+//   final Color color;
+//   final Color borderColor;
+
+//   const CustomBoxDecoration({
+//     Key? key,
+//     required this.radius,
+//     required this.color,
+//     required this.borderColor,
+//     this.width = 0,
+//     this.height = 0,
+//   });
+
+//   BoxDecoration baseBackgroundDecoration() {
+//     return BoxDecoration(
+//         color: color,
+//         borderRadius: BorderRadius.circular(radius),
+//         border: Border.fromBorderSide(
+//           BorderSide(
+//             color: borderColor,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
